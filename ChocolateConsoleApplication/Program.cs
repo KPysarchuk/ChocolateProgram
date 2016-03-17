0bc3164989b9dd421cf058006ac8952225a35ecb
Kostya <kot2206@gmail.com>
2016-03-17 16:08:45 +0200

    Finished commit

---
+++ ChocolateConsoleApplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chocolates;
+
+namespace ConsoleApplicationChocolate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the quantity of chocolate stashes:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            ChocolateStashes[] chocolatearr = new ChocolateStashes[n];
+            for (int i = 0; i < n; i++)
+            {
+                chocolatearr[i] = new ChocolateStashes();
+            }
+            Console.WriteLine("Enter the quantity of chocolates in each stash through the space:");
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(arr_temp, Int32.Parse);
+            for (int i = 0; i < n; i++)
+            {
+                chocolatearr[i].Quantity = a[i];
+            }
+            //Games.GameOfChocolate(n, chocolatearr);
+            Console.WriteLine(Games.GameOfChocolate(n, chocolatearr));
+
+        }
+    }
+}
---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace Chocolates
+{
+    public class ChocolateStashes
+    {
+        public int quantity;
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+    }
+
+    public class Games
+    {
+        public static int GameOfChocolate(int n, ChocolateStashes[] stashes)
+        {
+            int val = 0;
+            for (int i = 0; i < n; i++)
+            {
+                val ^= stashes[i].Quantity;
+            }
+            int answ = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if ((val ^ stashes[i].Quantity) < stashes[i].Quantity)
+                {
+                    answ++;
+                }
+            }
+            return answ;
+        }
+    }
+}
